40c2e95460edb2b2c0621dd35bd09d4fc5c39a7f
SarvamFonia <sarvamfonia@gmail.com>
2024-12-20 20:48:57 +0530

    signup functionality

--- Frontend/Destinify/src/routes/AppRoutes.jsx
+++ Frontend/Destinify/src/routes/AppRoutes.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import ExampleComponent from '../components/ExampleComponent';
 import { HomePage } from '../pages/HomePage/HomePage';
 import Login from '../components/Login/Login';
+import Signup from '../components/Signup/Signup'
 import DestinationPage from '../pages/DestinationPage/DestinationPage';
 
 const AppRoutes = () => (
@@ -12,6 +13,8 @@ const AppRoutes = () => (
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/destination" element={<DestinationPage />} />
+      <Route path="/signup" element={<Signup />} />
+      
     </Routes>
   </Router>
 );
